c01f41ca4e41c34f94c9c81ef80e249e3ea2a674
Sule, Nishant V <nsule@seas.harvard.edu>
2018-08-20 09:31:33 -0400

    added sliders and plots for noise distribution

--- CarFollowingK1D_bokehapp_embed.py
+++ CarFollowingK1D_bokehapp_embed.py
@@ -3,98 +3,141 @@ import time
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
 from bokeh.plotting import figure 
-from bokeh.layouts import row, column
-from bokeh.models.widgets import Button
+from bokeh.layouts import row, column, widgetbox
+from bokeh.models.widgets import Slider, Div
 from bokeh.server.server import Server
+from bokeh.models import Label
 
 # Initial values
-ProcNoise = 0.4
-MeasNoise = 0.8
-dist_size = 100
-car_size = 2
-car1_initpos = 10
-car2_initpos = 6
+dist_size = 20
+car_size = 1
+car1_initpos = 3
+car2_initpos = 0
 car1_vel = 4
 car2_initvel = 4
 car1_acc = 0
 car2_initacc = 0
 car2_maxacc = 2
 car2_maxbrake = 40
-dt = 0.01
-dist_sep = 2 * car_size
-arr_size = 50
+dt = 0.015
+timesteps = int((dist_size - car1_initpos) / car1_vel / dt)
+dist_sep = 3 * car_size
 
-# Initializing arrays for plotting
-timepoints = np.zeros(arr_size)
-timepoints.fill(np.nan)
-poscar1 = np.zeros(arr_size)
-poscar1.fill(np.nan)
-poscar1_est = np.zeros(arr_size)
-poscar1_est.fill(np.nan)
-poscar1_meas = np.zeros(arr_size)
-poscar1_meas.fill(np.nan)
-poscar2 = np.zeros([arr_size, 2])
-poscar2.fill(np.nan)
-car_sep = np.zeros([arr_size, 2])
-car_sep.fill(np.nan)
-car2_acc = np.zeros([arr_size, 2])
-car2_acc.fill(np.nan)
+timepoints = np.zeros(timesteps)
+poscar1 = np.zeros(timesteps)
+poscar1_est = np.zeros(timesteps)
+poscar1_meas = np.zeros(timesteps)
+poscar2 = np.zeros(timesteps)
+car_sep = np.zeros(timesteps)
+car2_acc = np.zeros(timesteps)
+poscar2_kf = np.zeros(timesteps)
+car_sep_kf = np.zeros(timesteps)
+car2_acc_kf = np.zeros(timesteps)
+noiserange = np.zeros(timesteps)
+pdf_proc = np.zeros(timesteps)
+pdf_meas = np.zeros(timesteps)
+
 
 def modify_doc(doc):
-# Setup plots
 
-    # Figure1
-    p1 = figure(plot_width=300, plot_height=400, x_axis_label='time', y_axis_label='position',
-                title='Car positions')
-    r1 = p1.circle(timepoints, poscar1, color='teal', legend='car1',
-                line_width=2)
-    r2 = p1.circle(timepoints, poscar1_meas, color='olivedrab', legend='car1 meas.',
-                line_width=2)
-    r3 = p1.circle(timepoints, poscar1_est, color='greenyellow', legend='car1 est.',
-                line_width=2)
-    r4 = p1.circle(timepoints, poscar2[:, 0], color='gold', legend='car2 no kf',
-                line_width=2)
-    r5 = p1.circle(timepoints, poscar2[:, 1], color='darkslategray', legend='car2 with kf',
-                line_width=2)
-    p1.legend.location = 'center_right'
+    # Initializing arrays for plotting
+    def InitPlotArrays():    
+        timepoints.fill(np.nan)
+        poscar1.fill(np.nan)
+        poscar1_est.fill(np.nan)
+        poscar1_meas.fill(np.nan)
+        poscar2.fill(np.nan)
+        car_sep.fill(np.nan)
+        car2_acc.fill(np.nan)
+        poscar2_kf.fill(np.nan)
+        car_sep_kf.fill(np.nan)
+        car2_acc_kf.fill(np.nan)
+        noiserange.fill(np.nan)
+        pdf_proc.fill(np.nan)
+        pdf_meas.fill(np.nan)
+
+    # Setup plots
+    InitPlotArrays()
+    
+    # Adding legends
+    p1 = figure(plot_width = 300, plot_height = 120, x_range=[-0.5, 20], y_range=[-1, 5])
+    colors = ['darkslategray', 'gold', 'crimson']
+    p1.rect([0.2, 0.2, 0.2], [4, 2, 0], width=1, height=1, color=colors, 
+                  width_units='data', height_units='data')
+    label1 = Label(x=1, y=3.3, x_units='data', y_units='data', text='Leading Car')
+    label2 = Label(x=1, y=1.3, x_units='data', y_units='data', text='Following Car')
+    label3 = Label(x=1, y=-0.7, x_units='data', y_units='data', text='Following Car with Kalman Filter')
+    p1.add_layout(label1)
+    p1.add_layout(label2)
+    p1.add_layout(label3)
+    p1.yaxis.visible = False
+    p1.ygrid.visible = False
+    p1.xaxis.visible = False
+    p1.xgrid.visible = False
     p1.toolbar.logo = None
     p1.toolbar_location = None
-    
-    # Figure 2
-    p2 = figure(plot_width=300, plot_height=400, x_axis_label='time', y_axis_label='separation',
-                title='Car separations', y_range=[0, 4 * dist_sep])
-    r6 = p2.circle(timepoints, car_sep[:, 0], color='gold', line_width=2, legend='no KF')
-    r7 = p2.circle(timepoints, car_sep[:, 1], color='darkslategray', line_width=2, legend='with KF')
-    p2.legend.location = 'center_right'
+
+    # Figure 1
+    p2 = figure(plot_width = 400, plot_height = 200, title='Noise Distributions')
+    r21 = p2.line(noiserange, pdf_proc, line_color='seagreen', line_width=4, legend='Process')
+    r22 = p2.line(noiserange, pdf_meas, line_color='greenyellow', line_width=4, legend='Measurement')
     p2.toolbar.logo = None
     p2.toolbar_location = None
-    
-    # Figure 3
-    p3 = figure(plot_width=300, plot_height=400, x_axis_label='time', y_axis_label='acceleration',
-                title='Car2 acceleration')
-    r8 = p3.circle(timepoints, car2_acc[:, 0], color='gold', line_width=2, legend='no KF')
-    r9 = p3.circle(timepoints, car2_acc[:, 1], color='darkslategray', line_width=2, legend='with KF')
-    p3.legend.location = 'bottom_center'
+    p2.legend.location = 'top_left'
+
+    # Figure 2
+    p3 = figure(plot_width=1000, plot_height=150, x_axis_label='position', title='Car Following Animation',
+                x_range=[0, dist_size], y_range=[-0.4, 1.0])
+    colors = ['darkslategray', 'gold', 'crimson']
+    r31 = p3.rect(x=[car1_initpos, car2_initpos, car2_initpos], y=[0.0, -0.2, 0.2], 
+                  width=car_size, height=8, color=colors, 
+                  width_units='data', height_units='screen')
+    p3.yaxis.visible = False
+    p3.ygrid.visible = False
     p3.toolbar.logo = None
     p3.toolbar_location = None
-    
-    # Figure 4
-    p4 = figure(plot_width=900, plot_height=200, x_axis_label='position', title='Car Following',
-                x_range=[0, dist_size])
-    colors = ['teal', 'gold', 'darkslategray']
-    r10 = p4.rect(x=[car1_initpos, car2_initpos, car2_initpos], y=[0, 0, 0], 
-                  width=2, height=10, color=colors, alpha=0.6,
-                  width_units='data', height_units='screen')
-    p4.yaxis.visible = False
-    p4.ygrid.visible = False
+
+    # Figure 3
+    p4 = figure(plot_width=500, plot_height=400, x_axis_label='time', y_axis_label='position',
+                title='Car Positions', x_range=[0, timesteps * dt], y_range=[0, dist_size])
+    r41 = p4.line(timepoints, poscar1, color='darkslategray', line_width=4)
+    r42 = p4.line(timepoints, poscar1_meas, color='royalblue', 
+                 legend='Measured position', line_width=2)
+    r43 = p4.line(timepoints, poscar1_est, color='skyblue', 
+                 legend='KF estimated position', line_width=2)
+    r44 = p4.line(timepoints, poscar2, color='gold', line_width=4)
+    r45 = p4.line(timepoints, poscar2_kf, color='crimson', line_width=4)
+    p4.legend.location = 'bottom_right'
     p4.toolbar.logo = None
     p4.toolbar_location = None
 
+    # Figure 4
+    p5 = figure(plot_width=500, plot_height=200, x_axis_label='time', y_axis_label='separation',
+                title='Car Separations', x_range=[0, timesteps * dt], y_range=[0, 4 * dist_sep])
+    r51 = p5.line(timepoints, car_sep, color='gold', line_width=4)
+    r52 = p5.line(timepoints, car_sep_kf, color='crimson', line_width=4)
+    p5.toolbar.logo = None
+    p5.toolbar_location = None
+    
+    # Figure 5
+    p6 = figure(plot_width=500, plot_height=200, x_axis_label='time', y_axis_label='acceleration',
+                title='Following Car Accelerations', x_range=[0, timesteps * dt], y_range=[-car2_maxbrake, car2_maxacc])
+    r61 = p6.line(timepoints, car2_acc, color='gold', line_width=4)
+    r62 = p6.line(timepoints, car2_acc_kf, color='crimson', line_width=4)
+    p6.toolbar.logo = None
+    p6.toolbar_location = None
+
     # Setup widgets
-    StartButton = Button(label='Start', width=10)
+    procnoise_slider = Slider(title='Process Noise', value=0.5, start=0.1, end=1.5, step=0.1)
+    measnoise_slider = Slider(title='Measurement Noise', value=0.5, start=0.1, end=1.5, step=0.1)
+    TextDisp = Div(text='''<b>Note:</b> Wait for the plots to stop updating before changing inputs.''')
 
     def RunCarFollowing(attr, old, new):        
+        # Get current widget values
+        PN = procnoise_slider.value
+        MN = measnoise_slider.value
         # Setup initial values
+        InitPlotArrays()
         pos1real = car1_initpos
         pos2 = car2_initpos
         pos2_kf = car2_initpos
@@ -104,6 +147,17 @@ def modify_doc(doc):
         acc2_kf = car2_initacc
         tt = 0
         realt = 0
+
+       # Update noise distributions
+        nlim = max(PN, MN)
+        global noiserange, pdf_proc, pdf_meas
+        noiserange = np.linspace(-4 * nlim, 4 * nlim, timesteps)
+        pdf_proc = 1 / (PN * np.sqrt(2 * np.pi)) * np.exp(-noiserange**2 / (2 * PN**2))
+        r21.data_source.data['x'] = noiserange
+        r21.data_source.data['y'] = pdf_proc
+        pdf_meas = 1 / (MN * np.sqrt(2 * np.pi)) * np.exp(-noiserange**2 / (2 * MN**2))
+        r22.data_source.data['x'] = noiserange
+        r22.data_source.data['y'] = pdf_meas
         
         # Initialize Kalman Filter
         f = KalmanFilter(dim_x=2, dim_z=1)
@@ -115,32 +169,23 @@ def modify_doc(doc):
         f.P = np.array([[1000.0, 0.0],
                         [0.0, 1000.0]])
         f.R = np.array([[5.0]])
-        f.Q = Q_discrete_white_noise(dim=2, dt=dt, var=ProcNoise)  # Process noise
+        f.Q = Q_discrete_white_noise(dim=2, dt=dt, var=PN)  # Process noise
         
         # Loop for updating positions
-        while True:
-            tt = np.mod(tt, arr_size - 1)
+        for tt in range(timesteps):        
     
             # Real position of leading car
             pos1real = pos1real + car1_vel * dt
     
-            # Applying periodic boundary 
-            pos1real = np.mod(pos1real, dist_size)
-            pos2 = np.mod(pos2, dist_size)
-            pos2_kf = np.mod(pos2_kf, dist_size)
-    
             # Measurement of position of leading car with noise
-            z = np.random.normal(pos1real, MeasNoise)
+            z = np.random.normal(pos1real, MN)
     
             # Kalman Filter predict and update steps
             f.predict()
             f.update(z)   
-    
+   
             # Following car update without Kalman Filter
             dx = z - pos2
-            # Applying minimum image criteria for periodic boundary
-            while dx > dist_size / 2: dx = dx - dist_size
-            while dx < -dist_size / 2: dx = dx + dist_size
             err = (dx - dist_sep) / dist_sep
             if err > 1.0: err = 1.0
             # Acceleration model
@@ -158,9 +203,6 @@ def modify_doc(doc):
     
             # Following car update with Kalman Filter
             dx_kf = f.x[0, 0] - pos2_kf
-            # Applying minimum image criteria for periodic boundary
-            while dx_kf > dist_size / 2: dx_kf = dx_kf - dist_size
-            while dx_kf < -dist_size / 2: dx_kf = dx_kf + dist_size
             err_kf = (dx_kf - dist_sep) / dist_sep
             if err_kf > 1.0: err_kf = 1.0
             # Acceleration model
@@ -175,52 +217,53 @@ def modify_doc(doc):
                 acc2_kf = 0
             # update position
             pos2_kf = pos2_kf + vel2_kf * dt + 0.5 * acc2_kf * dt**2
-    
-            # update counter
-            tt = tt + 1
-            realt = realt + 1
-    
+
             # Update arrays for plotting
             global timepoints, poscar1, poscar2, poscar1_est, car_sep
-            timepoints[tt] = realt * dt
+            timepoints[tt] = tt * dt
             poscar1[tt] = pos1real
             poscar1_est[tt] = f.x[0, 0]
             poscar1_meas[tt] = z
-            poscar2[tt, 0] = pos2
-            poscar2[tt, 1] = pos2_kf
-            car_sep[tt, 0] = pos1real - pos2
-            car_sep[tt, 1] = pos1real - pos2_kf  
-            car2_acc[tt, 0] = acc2
-            car2_acc[tt, 1] = acc2_kf
-    
+            poscar2[tt] = pos2
+            poscar2_kf[tt] = pos2_kf
+            car_sep[tt] = pos1real - pos2
+            car_sep_kf[tt] = pos1real - pos2_kf  
+            car2_acc[tt] = acc2
+            car2_acc_kf[tt] = acc2_kf
+
             # Update plots
             #first row
-            r10.data_source.data['x'] = [pos1real, pos2, pos2_kf]
+            r31.data_source.data['x'] = [pos1real, pos2, pos2_kf]
             #second row
-            r1.data_source.data['x'] = timepoints
-            r1.data_source.data['y'] = poscar1
-            r2.data_source.data['x'] = timepoints
-            r2.data_source.data['y'] = poscar1_meas
-            r3.data_source.data['x'] = timepoints
-            r3.data_source.data['y'] = poscar1_est
-            r4.data_source.data['x'] = timepoints
-            r4.data_source.data['y'] = poscar2[:, 0]
-            r5.data_source.data['x'] = timepoints
-            r5.data_source.data['y'] = poscar2[:, 1]
-            r6.data_source.data['x'] = timepoints
-            r6.data_source.data['y'] = car_sep[:, 0]
-            r7.data_source.data['x'] = timepoints
-            r7.data_source.data['y'] = car_sep[:, 1]
-            r8.data_source.data['x'] = timepoints
-            r8.data_source.data['y'] = car2_acc[:, 0]
-            r9.data_source.data['x'] = timepoints
-            r9.data_source.data['y'] = car2_acc[:, 1]
-
+            r41.data_source.data['x'] = timepoints
+            r41.data_source.data['y'] = poscar1
+            r42.data_source.data['x'] = timepoints
+            r42.data_source.data['y'] = poscar1_meas
+            r43.data_source.data['x'] = timepoints
+            r43.data_source.data['y'] = poscar1_est
+            r44.data_source.data['x'] = timepoints
+            r44.data_source.data['y'] = poscar2
+            r45.data_source.data['x'] = timepoints
+            r45.data_source.data['y'] = poscar2_kf
+            r51.data_source.data['x'] = timepoints
+            r51.data_source.data['y'] = car_sep
+            r52.data_source.data['x'] = timepoints
+            r52.data_source.data['y'] = car_sep_kf
+            r61.data_source.data['x'] = timepoints
+            r61.data_source.data['y'] = car2_acc
+            r62.data_source.data['x'] = timepoints
+            r62.data_source.data['y'] = car2_acc_kf
+    
             time.sleep(0.1)
 
-    StartButton.on_click(RunCarFollowing)
-    doc.add_root(column(StartButton, p4))
-    doc.add_root(row(p1, p2, p3))
+    # Setup callbacks
+    for d in [procnoise_slider, measnoise_slider]:
+        d.on_change('value', RunCarFollowing)
+ 
+    # Setup layout and add to document
+    wInputs = widgetbox(procnoise_slider, measnoise_slider, TextDisp)    
+
+    doc.add_root(column(row(p1, p2, wInputs), p3, row(p4, column(p5, p6))))
 
 server = Server({'/': modify_doc}, num_procs=1)
 server.start()
